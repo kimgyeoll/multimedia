05ec07d0664c5b6a0f359bd4c2862fad5dbd0581
김결 <kimgyeoll@yahoo.com>
2023-09-01 10:16:38 +0900

    multimedia first push

---
+++ Multimedia/Multimedia/MultimediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultimediaApp.swift
+//  Multimedia
+//
+//  Created by kimgyeoll on 2023/09/01.
+//
+
+import SwiftUI
+
+@main
+struct MultimediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
