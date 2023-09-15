f17ce0624470977a396a9b6e47a1fbc641865a1b
김결 <kimgyeoll@yahoo.com>
2023-09-15 14:16:58 +0900

    레이아웃 수정

--- multimedia/multimedia/ViewController.swift
+++ multimedia/multimedia/ViewController.swift
@@ -9,12 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pictureImageView: UIImageView!
+    
+    var currentIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func prevButtonAction(_ sender: UIButton) {
+        currentIndex -= 1
+        if currentIndex < 0 {
+            currentIndex = 2
+        }
+        pictureImageView.image = UIImage(named: "\(currentIndex)")
+    }
+ 
+    @IBAction func nextButtonAction() {
+        currentIndex += 1
+        if currentIndex > 2 {
+            currentIndex = 0
+        }
+        pictureImageView.image = UIImage(named: "\(currentIndex)")
+    }
 }
 
 
